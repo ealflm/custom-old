8440d3ceaa0a9d2c11112b5a20df306db6e51b92
= <=>
2022-12-19 21:43:22 +0700

    Format and mappings

--- init.lua
+++ init.lua
@@ -3,8 +3,14 @@ require "custom.options"
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd({"BufWritePre"}, {
-    pattern = {"*.dart", "*.lua"},
-    callback = function() vim.lsp.buf.format {async = true} end
+  pattern = {"*.dart", "*.lua"},
+  callback = function() vim.lsp.buf.format {async = true} end
 })
 
 autocmd({"FocusGained"}, {pattern = {"*.*"}, command = "checktime"})
+
+-- Mapping <C-n>, <C-p> with up and down to get feature of up and down
+vim.cmd [[
+  cnoremap <C-p> <Up>
+  cnoremap <C-n> <Down>
+]]
--- options.lua
+++ options.lua
@@ -1,12 +1,13 @@
 local opt = vim.opt
 local g = vim.g
 
+g.neovide_input_macos_alt_is_meta = true
+g.neovide_cursor_vfx_mode = "torpedo"
+
 -- opt.guifont = { "Hack Nerd Font Mono:h14" }
 opt.mouse = ""
 opt.wrap = false
 opt.shell = "fish"
 opt.clipboard = ""
 opt.autoread = true
-
-g.neovide_input_macos_alt_is_meta = true
-g.neovide_cursor_vfx_mode = "torpedo"
+opt.history = 999
